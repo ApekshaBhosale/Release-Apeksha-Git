ab6e87e308a0a30d41596bbe653df4e8c6050171
Apeksha Bhosale <apeksha@appsmith.com>
2024-11-14 06:05:07 +0000

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,16 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		return "hey!";
+	},
+	async myFun2 () {
+		return Api1.run();
+	},
+	myFun11 () {
+		return "hey!";
+	},
+	myFun12 () {
+		return "hey!";
+	},
+}
